c18886f985d6d64520700b8dfe1061ac37eadbb3
Esetobore <esetobore1@gmail.com>
2022-12-24 20:11:23 +0100

    Firebase AutoMl Setup

---
+++ app/src/main/java/com/example/currencyrecognition/splash/MainSplash.kt
@@ -0,0 +1,12 @@
+package com.example.currencyrecognition.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.currencyrecognition.R
+
+class MainSplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_splash)
+    }
+}
