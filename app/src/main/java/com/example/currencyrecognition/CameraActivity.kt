830ef7ef533ce22ca424c4ac44c9d0237dddd3fc
Esetobore <esetobore1@gmail.com>
2023-01-04 00:02:31 +0100

    camera xml complete, kt file working on

--- app/src/main/java/com/example/currencyrecognition/CameraActivity.kt
+++ app/src/main/java/com/example/currencyrecognition/CameraActivity.kt
@@ -1,11 +1,31 @@
 package com.example.currencyrecognition
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
     }
+
+
+
+    private fun permissions(){
+        val checkSelfPermission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)==
+                PackageManager.PERMISSION_GRANTED
+        if (checkSelfPermission) {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, 1)
+        } else {
+            //Request camera permission if we don't have it.
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+        }
+    }
+
 }
