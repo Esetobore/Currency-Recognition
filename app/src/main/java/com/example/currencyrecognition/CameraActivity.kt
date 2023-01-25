579bcebde0f3bcc60846f1caf67af9ecbfd73bd2
Esetobore <esetobore1@gmail.com>
2023-01-25 21:32:53 +0100

    model now produces results into the app textview work on audio next

--- app/src/main/java/com/example/currencyrecognition/CameraActivity.kt
+++ app/src/main/java/com/example/currencyrecognition/CameraActivity.kt
@@ -1,23 +1,19 @@
 package com.example.currencyrecognition
 
-import android.R.attr
-import android.app.Activity
-import android.app.Instrumentation.ActivityResult
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.media.ThumbnailUtils
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.currencyrecognition.ml.ModelUnquant
 import kotlinx.android.synthetic.main.activity_camera.*
-import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
-import java.util.Collections.min
-import kotlin.math.min
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 class CameraActivity : AppCompatActivity() {
@@ -57,12 +53,49 @@ class CameraActivity : AppCompatActivity() {
             imageView.setImageBitmap(image)
 
             image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+
             imgClassificationByModel(image)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun imgClassificationByModel(image: Bitmap) {
+
+        val model = ModelUnquant.newInstance(applicationContext)
+
+        // Creates inputs for reference.
+        val inputFeature0 =
+            TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+
+        // bitmap image to bytebuffer
+        val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+
+        // get the array of 224 * 224 pixels in the captured image
+        val intValues = IntArray(imageSize * imageSize)
+        image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+
+        // iterate over pixels and extract R, G, and B values. Add to bytebuffer. as seen from the modelTFLite
+        var pixel = 0
+        for (i in 0 until imageSize) {
+            for (j in 0 until imageSize) {
+                val `val` = intValues[pixel++] // RGB
+                byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+            }
+        }
+
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        result.text = outputFeature0.toString()
+
+        // Releases model resources if no longer used.
+        model.close()
+
 
     }
 
